bf2912892be778cf0d0bfa6becf935c6b895d4af
MohamedNaama91200 <genormica6@gmail.com>
2022-09-15 11:14:26 +0200

    Deuxieme commit du projet tp2

---
+++ src/Entity/Inventaire.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InventaireRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=InventaireRepository::class)
+ */
+class Inventaire
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Objet::class, mappedBy="inventaire")
+     */
+    private $description;
+
+    public function __construct()
+    {
+        $this->description = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection<int, Objet>
+     */
+    public function getDescription(): Collection
+    {
+        return $this->description;
+    }
+
+    public function addDescription(Objet $description): self
+    {
+        if (!$this->description->contains($description)) {
+            $this->description[] = $description;
+            $description->setInventaire($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDescription(Objet $description): self
+    {
+        if ($this->description->removeElement($description)) {
+            // set the owning side to null (unless already changed)
+            if ($description->getInventaire() === $this) {
+                $description->setInventaire(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Objet.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ObjetRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ObjetRepository::class)
+ */
+class Objet
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Titre;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $Nb_de_tours;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $Neuf;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $Annee;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Album;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Durée;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Inventaire::class, inversedBy="description")
+     */
+    private $inventaire;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): self
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+
+    public function getNbDeTours(): ?int
+    {
+        return $this->Nb_de_tours;
+    }
+
+    public function setNbDeTours(int $Nb_de_tours): self
+    {
+        $this->Nb_de_tours = $Nb_de_tours;
+
+        return $this;
+    }
+
+    public function isNeuf(): ?bool
+    {
+        return $this->Neuf;
+    }
+
+    public function setNeuf(bool $Neuf): self
+    {
+        $this->Neuf = $Neuf;
+
+        return $this;
+    }
+
+    public function getAnnee(): ?int
+    {
+        return $this->Annee;
+    }
+
+    public function setAnnee(int $Annee): self
+    {
+        $this->Annee = $Annee;
+
+        return $this;
+    }
+
+    public function getAlbum(): ?string
+    {
+        return $this->Album;
+    }
+
+    public function setAlbum(string $Album): self
+    {
+        $this->Album = $Album;
+
+        return $this;
+    }
+
+    public function getDurée(): ?string
+    {
+        return $this->Durée;
+    }
+
+    public function setDurée(string $Durée): self
+    {
+        $this->Durée = $Durée;
+
+        return $this;
+    }
+
+    public function getInventaire(): ?Inventaire
+    {
+        return $this->inventaire;
+    }
+
+    public function setInventaire(?Inventaire $inventaire): self
+    {
+        $this->inventaire = $inventaire;
+
+        return $this;
+    }
+}
